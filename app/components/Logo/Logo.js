62aff64c45e5b5335f33b070ea9852fa06acd35b
shuma <usman.rajab@gmail.com>
2017-09-26 17:48:04 +0200

    Animating the logo part 1

--- app/components/Logo/Logo.js
+++ app/components/Logo/Logo.js
@@ -1,19 +1,30 @@
-import React from 'react';
-import { View, Image, Text } from 'react-native';
+import React, { Component } from 'react';
+import { View, Image, Text, Keyboard } from 'react-native';
 
 import styles from './styles';
 
-const Logo = () => (
-  <View style={styles.container}>
-    <Image
-      resizeMode="contain"
-      style={styles.containerImage}
-      source={require('./images/background.png')}
-    >
-      <Image resizeMode="contain" style={styles.image} source={require('./images/logo.png')} />
-    </Image>
-    <Text style={styles.text}>Currency Converter</Text>
-  </View>
-);
+export default class Logo extends Component {
+  componentDidMount() {
+    this.keyboardShowListener = Keyboard.addListener('keyboardWillShow', this.keyboardShow);
+    this.keyboardHideListener = Keyboard.addListener('keyboardWillHide', this.keyboardHide);
+  }
+  componentWillUnmount() {}
 
-export default Logo;
+  keyboardShow = () => {};
+  keyboardHide = () => {};
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+          resizeMode="contain"
+          style={styles.containerImage}
+          source={require('./images/background.png')}
+        >
+          <Image resizeMode="contain" style={styles.image} source={require('./images/logo.png')} />
+        </Image>
+        <Text style={styles.text}>Currency Converter</Text>
+      </View>
+    );
+  }
+}

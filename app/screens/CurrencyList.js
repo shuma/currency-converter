09fd512e32dc08cf4622addd45c9a8d9edc3fefe
shuma <usman.rajab@gmail.com>
2017-10-07 18:36:35 +0200

    currency list screen listitem

--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { FlatList, StatusBar, View } from 'react-native';
+import { connect } from 'react-redux';
 
 import currencies from '../data/currencies';
+import { changeBaseCurrency, changeQuoteCurrency } from '../actions/currencies';
 import { ListItem, Separator } from '../components/List';
 
 const TEMP_CURRENT_CURRENCY = 'CAD';
 
-export default class CurrencyList extends Component {
+class CurrencyList extends Component {
   static propTypes = {
     navigation: PropTypes.object,
+    dispatch: PropTypes.func,
   };
-  handlePress = () => {
+  handlePress = (currency) => {
+    const { type } = this.props.navigation.state.params;
+    if (type === 'base') {
+      this.props.dispatch(changeBaseCurrency(currency));
+    } else if (type === 'quote') {
+      this.props.dispatch(changeQuoteCurrency(currency));
+    }
     this.props.navigation.goBack(null);
   };
 
@@ -25,7 +34,7 @@ export default class CurrencyList extends Component {
             <ListItem
               text={item}
               selected={item === TEMP_CURRENT_CURRENCY}
-              onPress={this.handlePress}
+              onPress={() => this.handlePress(item)}
             />
           )}
           keyExtractor={item => item}
@@ -35,3 +44,4 @@ export default class CurrencyList extends Component {
     );
   }
 }
+export default connect()(CurrencyList);
